e6c75068074a91ae0e7c36887634aebf89beedd2
Poppy Curtis <poppy.curtis@gmail.com>
2019-02-06 10:03:39 +0000

    Updates

---
+++ precache-manifest.a48a254707ea06a40ff8e4f627286c60.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "af04a783900b5cf4b204",
+    "url": "/portfolio/static/css/main.076f0743.chunk.css"
+  },
+  {
+    "revision": "af04a783900b5cf4b204",
+    "url": "/portfolio/static/js/main.af04a783.chunk.js"
+  },
+  {
+    "revision": "235b43a19dc7bee9a861",
+    "url": "/portfolio/static/js/1.235b43a1.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "91577282b84e9e8c1a92eef959ab7066",
+    "url": "/portfolio/static/media/poppy.91577282.JPG"
+  },
+  {
+    "revision": "a5573d74210aee456a0a073e110b49d2",
+    "url": "/portfolio/static/media/ux.a5573d74.png"
+  },
+  {
+    "revision": "1e6a8cd502d145947e1788fe3aab8b5f",
+    "url": "/portfolio/static/media/database.1e6a8cd5.png"
+  },
+  {
+    "revision": "0422b0f6d3f44fdefc82be01946e8538",
+    "url": "/portfolio/static/media/responsive.0422b0f6.png"
+  },
+  {
+    "revision": "1004a1e76cdd4c447704e78d9ca9a7eb",
+    "url": "/portfolio/static/media/cover.1004a1e7.jpg"
+  },
+  {
+    "revision": "217e476d7eec7aede306e45dfd63f7f1",
+    "url": "/portfolio/index.html"
+  }
+];
